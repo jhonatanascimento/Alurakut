3c175801955c7ebbb3c5c76617abee9543aaa5e4
Jhonata Nascimento <87243089+jhonatanascimento@users.noreply.github.com>
2021-07-15 18:57:18 -0300

    Profilesidebar Mudanças

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,63 @@
-import MainGrid from '../src/components/MainGrid'
-import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
-import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import MainGrid from "../src/components/MainGrid";
+import Box from "../src/components/Box";
+import {
+  AlurakutMenu,
+  OrkutNostalgicIconSet,
+  AlurakutProfileSidebarMenuDefault
+} from "../src/lib/AlurakutCommons";
+import { ProfileRelationsBoxWrapper } from "../src/components/ProfileRelations";
 
 function ProfileSidebar(propriedades) {
-  console.log(propriedades);
   return (
     <Box>
-      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <img
+        alt="imagem"
+        src={`https://github.com/${propriedades.githubUser}.png`}
+        style={{ borderRadius: "8px" }}
+      />
+      <hr />
+      <a className="" href={`Https://github.com/${propriedades.githubUser}`}>
+        @{propriedades.githubUser}
+      </a>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
-  )
+  );
 }
 
 export default function Home() {
-  const usuarioAleatorio = 'jhonatanascimento';
+  const usuarioAleatorio = "jhonatanascimento";
   const pessoasFavoritas = [
-    'juunegreiros',
-    'omariosouto',
-    'peas',
-    'rafaballerini',
-    'marcobrunodev',
-    'felipefialho'
-  ]
+    "juunegreiros",
+    "omariosouto",
+    "peas",
+    "rafaballerini",
+    "marcobrunodev",
+    "felipefialho"
+  ];
 
   return (
     <>
       <AlurakutMenu />
       <MainGrid>
         {/* <Box style="grid-area: profileArea;"> */}
-        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <div className="profileArea" style={{ gridArea: "profileArea" }}>
           <ProfileSidebar githubUser={usuarioAleatorio} />
         </div>
-        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <div className="welcomeArea" style={{ gridArea: "welcomeArea" }}>
           <Box>
-            <h1 className="title" style={{textAlign:'center'}}>
-              Bem vindo(a) 
+            <h1 className="title" style={{ textAlign: "center" }}>
+              Bem vindo(a)
             </h1>
 
             <OrkutNostalgicIconSet />
           </Box>
         </div>
-        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <div
+          className="profileRelationsArea"
+          style={{ gridArea: "profileRelationsArea" }}
+        >
           <ProfileRelationsBoxWrapper>
             <h2 className="smallTitle">
               Pessoas da comunidade ({pessoasFavoritas.length})
@@ -55,12 +72,12 @@ export default function Home() {
                       <span>{itemAtual}</span>
                     </a>
                   </li>
-                )
+                );
               })}
             </ul>
           </ProfileRelationsBoxWrapper>
         </div>
       </MainGrid>
     </>
-  )
+  );
 }
